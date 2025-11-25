ad0d7725b88ae514a4a5d486f4ceb0c6992a0cfa
DotHashir <dothashir@gmail.com>
2025-11-25 14:32:10 +0500

    "Basic setup for the project that shows menu"

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void showMenu(); // funtcion prototype
+
+int main()
+{
+    int choice;
+    do
+    { // a loop that keeps asking the user which game they want to play until they exit it.,
+        showMenu();
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 0:
+            cout << "Thank you for playing at Game Hub!"; // exits loop
+            break;
+        default:
+            cout << "Invalid input. Please choose a number between 0-6."; // shows that the input wasnt one that is defined in the code
+        }
+        cout << endl;
+    } while (choice != 0);
+}
+
+void showMenu()
+{ // function that shows which game can be played in the app
+    cout << "=============================\n";
+    cout << "        GAME HUB MENU      \n";
+    cout << "=============================\n";
+    cout << "1. Chess\n";
+    cout << "2. Hangman\n";
+    cout << "3. Guess the Number\n";
+    cout << "4. Head or Tails\n";
+    cout << "5. Tic Tac Toe\n";
+    cout << "6. Connect 4\n";
+    cout << "0. Exit\n";
+    cout << "=============================\n";
+    cout << "Enter your choice: ";
+}
